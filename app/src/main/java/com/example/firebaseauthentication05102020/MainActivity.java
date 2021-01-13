9c69a35b2e7c0913a1db497cd62779bb81efcf46
phatdroid94 <phatdroid94@gmail.com>
2021-01-13 20:27:55 +0700

    update

--- app/src/main/java/com/example/firebaseauthentication05102020/MainActivity.java
+++ app/src/main/java/com/example/firebaseauthentication05102020/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.firebaseauthentication05102020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mEdtTk,mEdtMk,mEdtPhone;
+    Button mBtnDangky,mBtnDangnhap,mBtnXacThuc,mBtnCapnhat,mBtnDangkyPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEdtMk = findViewById(R.id.edittextPassword);
+        mEdtTk = findViewById(R.id.edittextEmail);
+        mEdtPhone = findViewById(R.id.edittextPhone);
+        mBtnDangky = findViewById(R.id.buttonDangky);
+        mBtnDangnhap = findViewById(R.id.buttonDangnhap);
+        mBtnXacThuc = findViewById(R.id.buttonVerification);
+        mBtnCapnhat = findViewById(R.id.button_updatepassword);
+        mBtnDangkyPhone = findViewById(R.id.buttonRegisterPhone);
+
     }
 }
