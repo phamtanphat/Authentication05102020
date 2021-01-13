27375cc9642ce3cdeaf8eb197936ea805aa6bbdd
phatdroid94 <phatdroid94@gmail.com>
2021-01-13 21:02:37 +0700

    update password

--- app/src/main/java/com/example/firebaseauthentication05102020/MainActivity.java
+++ app/src/main/java/com/example/firebaseauthentication05102020/MainActivity.java
@@ -116,7 +116,6 @@ public class MainActivity extends AppCompatActivity {
                                 }
                             }
                         });
-
             }
         });
 
